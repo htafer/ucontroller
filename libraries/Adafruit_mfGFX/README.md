0e0590174a4d5a47addeae5f2031177687506eb9
htafer <htafer@htafer@gmail.com>
2016-01-02 12:35:57 +0100

    add all sketches available

---
+++ libraries/Adafruit_mfGFX/README.md
@@ -0,0 +1,63 @@
+Adafruit_mfGFX_Library
+======================
+
+Multifont version of Adafruit_GFX library, with fonts loaded as-needed. Supports several fonts, but only loads the ones you use into flash memory.
+
+This library supports any compatible Adafruit_GFX display driver. The free TheDotFactory Windows program is recommended for created data compatible with the Adafruit_mfGFX library.
+
+Please see the README.pdf files for instructions on creating and adding new fonts.
+
+Fonts:
+------
+
+| Font Name  | Size | Include |
+| ---------- | ---- | ------- |
+| Arial      | 8pt  | #define font_arial_8 1 |
+| Dejavu     | 24pt | #define font_dejavu_24 2 |
+| Dejavu     | 48pt | #define font_dejavu_48 3 |
+| Gausshauss | 22px | #define font_gausshauss_22 4 |
+| Lucida     | 24pt | #define font_lucida_24 5 |
+| Marlboro   | 23pt | #define font_marlboro_23 6 |
+| Marlboro   | 46pt | #define font_marlboro_46 7 |
+| Nearly     | 24px | #define font_nearly_24 8 |
+| Parsons    | 46px | #define font_parsons_46 9 |
+| Republica  | 22px | #define font_republica_22 10 |
+| Tahoma     | 24px | #define font_tahoma_24 11 |
+| Titania    | 24px | #define font_titania_24 12 |
+| Titania    | 46px | #define font_titania_46 13 |
+
+
+Example:
+--------
+
+```
+// see Adafruit_mfGFX.h for a complete list of supported fonts.
+// Note: Each defined font MUST have a unique ID. It doesn't matter what the
+// integer ID is, as long as it's unique. For instance:
+#define font_gausshauss_22   1
+#define font_marlboro_48     2
+#define font_republica_22    3
+
+#include <Adafruit_mfGFX.h>
+#include <Adafruit_SSD1306.h>
+#include <SPI.h>
+#include <Wire.h>
+
+void writeMedium(const char *str, uint8_t line) {
+  display.setFont(font_gausshauss_22);
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 16 + line * 24);
+
+  int i = 0;
+  while(i < 20 && str[i] != '\0') {
+    display.write(str[i]);
+    i++;
+  }
+}
+
+void setup(void) {
+    // initialize display, then...
+    writeMedium("Hi!", 0);
+}
+```
---
+++ ssd1306_128x64_i2c/ssd1306_128x64_i2c.ino
@@ -0,0 +1,403 @@
+/*********************************************************************
+This is an example for our Monochrome OLEDs based on SSD1306 drivers
+
+  Pick one up today in the adafruit shop!
+  ------> http://www.adafruit.com/category/63_98
+
+This example is for a 128x64 size display using I2C to communicate
+3 pins are required to interface (2 I2C and one reset)
+
+Adafruit invests time and resources providing this open source code, 
+please support Adafruit and open-source hardware by purchasing 
+products from Adafruit!
+
+Written by Limor Fried/Ladyada  for Adafruit Industries.  
+BSD license, check license.txt for more information
+All text above, and the splash screen must be included in any redistribution
+*********************************************************************/
+
+//#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+#define NUMFLAKES 10
+#define XPOS 0
+#define YPOS 1
+#define DELTAY 2
+
+
+#define LOGO16_GLCD_HEIGHT 16 
+#define LOGO16_GLCD_WIDTH  16 
+static const unsigned char PROGMEM logo16_glcd_bmp[] =
+{ B00000000, B11000000,
+  B00000001, B11000000,
+  B00000001, B11000000,
+  B00000011, B11100000,
+  B11110011, B11100000,
+  B11111110, B11111000,
+  B01111110, B11111111,
+  B00110011, B10011111,
+  B00011111, B11111100,
+  B00001101, B01110000,
+  B00011011, B10100000,
+  B00111111, B11100000,
+  B00111111, B11110000,
+  B01111100, B11110000,
+  B01110000, B01110000,
+  B00000000, B00110000 };
+
+#if (SSD1306_LCDHEIGHT != 64)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
+
+void setup()   {                
+  Serial.begin(115200);
+  Wire.begin();
+  //Check if device is found at adress 0x3C
+  Wire.beginTransmission(0x3c);
+  byte error;
+  error=Wire.endTransmission();
+  if(error==0){
+    Serial.println("Screen found at 0x3C");
+  }else{
+    Serial.println("No screen found at 0x3C");
+  }
+  // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
+  //display.begin(SSD1306_SWITCHCAPVCC, 0x3c);  // initialize with the I2C addr 0x3D (for the 128x64)
+
+  display.begin(SSD1306_SWITCHCAPVCC,0x3c);
+   display.ssd1306_command(0xae);    //display off                                                                                                                         
+  display.ssd1306_command(0xa6);            //Set Normal Display (default)                                                                                                
+    // Adafruit Init sequence for 128x64 OLED module                                                                                                          
+    display.ssd1306_command(0xAE);             //DISPLAYOFF                                                                                                               
+    display.ssd1306_command(0xD5);            //SETDISPLAYCLOCKDIV                                                                                                        
+    display.ssd1306_command(0x80);            // the suggested ratio 0x80                                                                                                 
+    display.ssd1306_command(0xA8);            //SSD1306_SETMULTIPLEX                                                                                                      
+    display.ssd1306_command(0x3F);
+    display.ssd1306_command(0xD3);            //SETDISPLAYOFFSET                                                                                                          
+    display.ssd1306_command(0x0);             //no offset                                                                                                                 
+    display.ssd1306_command(0x40 | 0x0);      //SETSTARTLINE                                                                                                              
+    display.ssd1306_command(0x8D);            //CHARGEPUMP                                                                                                                
+    display.ssd1306_command(0x14);
+    display.ssd1306_command(0x20);             //MEMORYMODE                                                                                                               
+    display.ssd1306_command(0x00);             //0x0 act like ks0108                                                                                                      
+
+    //display.ssd1306_command(0xA0 | 0x1);      //SEGREMAP   //Rotate screen 180 deg                                                                                      
+    display.ssd1306_command(0xA0);
+
+    //display.ssd1306_command(0xC8);            //COMSCANDEC  Rotate screen 180 Deg                                                                                       
+    display.ssd1306_command(0xC0);
+
+    display.ssd1306_command(0xDA);            //0xDA                                                                                                                      
+    display.ssd1306_command(0x12);           //COMSCANDEC                                                                                                                 
+    display.ssd1306_command(0x81);           //SETCONTRAS                                                                                                                 
+    display.ssd1306_command(0xCF);           //                                                                                                                           
+    display.ssd1306_command(0xd9);          //SETPRECHARGE                                                                                                                
+    display.ssd1306_command(0xF1);
+    display.ssd1306_command(0xDB);        //SETVCOMDETECT                                                                                                                 
+    display.ssd1306_command(0x40);
+    display.ssd1306_command(0xA4);        //DISPLAYALLON_RESUME                                                                                                           
+    display.ssd1306_command(0xA6);        //NORMALDISPLAY                              
+
+
+  delay(2000);
+
+  display.clearDisplay();
+  // init done
+  Serial.println("Setup done");
+  // Show image buffer on the display hardware.
+  // Since the buffer is intialized with an Adafruit splashscreen
+  // internally, this will display the splashscreen.
+  display.display();
+  delay(2000);
+
+  // Clear the buffer.
+  display.clearDisplay();
+
+  // draw a single pixel
+  display.drawPixel(10, 10, WHITE);
+  // Show the display buffer on the hardware.
+  // NOTE: You _must_ call display after making any drawing commands
+  // to make them visible on the display hardware!
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+
+  // draw many lines
+  testdrawline();
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+
+  // draw rectangles
+  testdrawrect();
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+
+  // draw multiple rectangles
+  testfillrect();
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+
+  // draw mulitple circles
+  testdrawcircle();
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+
+  // draw a white circle, 10 pixel radius
+  display.fillCircle(display.width()/2, display.height()/2, 10, WHITE);
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+
+  testdrawroundrect();
+  delay(2000);
+  display.clearDisplay();
+
+  testfillroundrect();
+  delay(2000);
+  display.clearDisplay();
+
+  testdrawtriangle();
+  delay(2000);
+  display.clearDisplay();
+   
+  testfilltriangle();
+  delay(2000);
+  display.clearDisplay();
+
+  // draw the first ~12 characters in the font
+  testdrawchar();
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+
+  // draw scrolling text
+  testscrolltext();
+  delay(2000);
+  display.clearDisplay();
+
+  // text display tests
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.println("Hello, world!");
+  display.setTextColor(BLACK, WHITE); // 'inverted' text
+  display.println(3.141592);
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.print("0x"); display.println(0xDEADBEEF, HEX);
+  display.display();
+  delay(2000);
+
+  // miniature bitmap display
+  display.clearDisplay();
+  display.drawBitmap(30, 16,  logo16_glcd_bmp, 16, 16, 1);
+  display.display();
+
+  // invert the display
+  display.invertDisplay(true);
+  delay(1000); 
+  display.invertDisplay(false);
+  delay(1000); 
+
+  // draw a bitmap icon and 'animate' movement
+  testdrawbitmap(logo16_glcd_bmp, LOGO16_GLCD_HEIGHT, LOGO16_GLCD_WIDTH);
+}
+
+
+void loop() {
+  
+}
+
+
+void testdrawbitmap(const uint8_t *bitmap, uint8_t w, uint8_t h) {
+  uint8_t icons[NUMFLAKES][3];
+ 
+  // initialize
+  for (uint8_t f=0; f< NUMFLAKES; f++) {
+    icons[f][XPOS] = random(display.width());
+    icons[f][YPOS] = 0;
+    icons[f][DELTAY] = random(5) + 1;
+    
+    Serial.print("x: ");
+    Serial.print(icons[f][XPOS], DEC);
+    Serial.print(" y: ");
+    Serial.print(icons[f][YPOS], DEC);
+    Serial.print(" dy: ");
+    Serial.println(icons[f][DELTAY], DEC);
+  }
+
+  while (1) {
+    // draw each icon
+    for (uint8_t f=0; f< NUMFLAKES; f++) {
+      display.drawBitmap(icons[f][XPOS], icons[f][YPOS], logo16_glcd_bmp, w, h, WHITE);
+    }
+    display.display();
+    delay(200);
+    
+    // then erase it + move it
+    for (uint8_t f=0; f< NUMFLAKES; f++) {
+      display.drawBitmap(icons[f][XPOS], icons[f][YPOS],  logo16_glcd_bmp, w, h, BLACK);
+      // move it
+      icons[f][YPOS] += icons[f][DELTAY];
+      // if its gone, reinit
+      if (icons[f][YPOS] > display.height()) {
+	icons[f][XPOS] = random(display.width());
+	icons[f][YPOS] = 0;
+	icons[f][DELTAY] = random(5) + 1;
+      }
+    }
+   }
+}
+
+
+void testdrawchar(void) {
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+
+  for (uint8_t i=0; i < 168; i++) {
+    if (i == '\n') continue;
+    display.write(i);
+    if ((i > 0) && (i % 21 == 0))
+      display.println();
+  }    
+  display.display();
+}
+
+void testdrawcircle(void) {
+  for (int16_t i=0; i<display.height(); i+=2) {
+    display.drawCircle(display.width()/2, display.height()/2, i, WHITE);
+    display.display();
+  }
+}
+
+void testfillrect(void) {
+  uint8_t color = 1;
+  for (int16_t i=0; i<display.height()/2; i+=3) {
+    // alternate colors
+    display.fillRect(i, i, display.width()-i*2, display.height()-i*2, color%2);
+    display.display();
+    color++;
+  }
+}
+
+void testdrawtriangle(void) {
+  for (int16_t i=0; i<min(display.width(),display.height())/2; i+=5) {
+    display.drawTriangle(display.width()/2, display.height()/2-i,
+                     display.width()/2-i, display.height()/2+i,
+                     display.width()/2+i, display.height()/2+i, WHITE);
+    display.display();
+  }
+}
+
+void testfilltriangle(void) {
+  uint8_t color = WHITE;
+  for (int16_t i=min(display.width(),display.height())/2; i>0; i-=5) {
+    display.fillTriangle(display.width()/2, display.height()/2-i,
+                     display.width()/2-i, display.height()/2+i,
+                     display.width()/2+i, display.height()/2+i, WHITE);
+    if (color == WHITE) color = BLACK;
+    else color = WHITE;
+    display.display();
+  }
+}
+
+void testdrawroundrect(void) {
+  for (int16_t i=0; i<display.height()/2-2; i+=2) {
+    display.drawRoundRect(i, i, display.width()-2*i, display.height()-2*i, display.height()/4, WHITE);
+    display.display();
+  }
+}
+
+void testfillroundrect(void) {
+  uint8_t color = WHITE;
+  for (int16_t i=0; i<display.height()/2-2; i+=2) {
+    display.fillRoundRect(i, i, display.width()-2*i, display.height()-2*i, display.height()/4, color);
+    if (color == WHITE) color = BLACK;
+    else color = WHITE;
+    display.display();
+  }
+}
+   
+void testdrawrect(void) {
+  for (int16_t i=0; i<display.height()/2; i+=2) {
+    display.drawRect(i, i, display.width()-2*i, display.height()-2*i, WHITE);
+    display.display();
+  }
+}
+
+void testdrawline() {  
+  for (int16_t i=0; i<display.width(); i+=4) {
+    display.drawLine(0, 0, i, display.height()-1, WHITE);
+    display.display();
+  }
+  for (int16_t i=0; i<display.height(); i+=4) {
+    display.drawLine(0, 0, display.width()-1, i, WHITE);
+    display.display();
+  }
+  delay(250);
+  
+  display.clearDisplay();
+  for (int16_t i=0; i<display.width(); i+=4) {
+    display.drawLine(0, display.height()-1, i, 0, WHITE);
+    display.display();
+  }
+  for (int16_t i=display.height()-1; i>=0; i-=4) {
+    display.drawLine(0, display.height()-1, display.width()-1, i, WHITE);
+    display.display();
+  }
+  delay(250);
+  
+  display.clearDisplay();
+  for (int16_t i=display.width()-1; i>=0; i-=4) {
+    display.drawLine(display.width()-1, display.height()-1, i, 0, WHITE);
+    display.display();
+  }
+  for (int16_t i=display.height()-1; i>=0; i-=4) {
+    display.drawLine(display.width()-1, display.height()-1, 0, i, WHITE);
+    display.display();
+  }
+  delay(250);
+
+  display.clearDisplay();
+  for (int16_t i=0; i<display.height(); i+=4) {
+    display.drawLine(display.width()-1, 0, 0, i, WHITE);
+    display.display();
+  }
+  for (int16_t i=0; i<display.width(); i+=4) {
+    display.drawLine(display.width()-1, 0, i, display.height()-1, WHITE); 
+    display.display();
+  }
+  delay(250);
+}
+
+void testscrolltext(void) {
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(10,0);
+  display.clearDisplay();
+  display.println("scroll");
+  display.display();
+ 
+  display.startscrollright(0x00, 0x0F);
+  delay(2000);
+  display.stopscroll();
+  delay(1000);
+  display.startscrollleft(0x00, 0x0F);
+  delay(2000);
+  display.stopscroll();
+  delay(1000);    
+  display.startscrolldiagright(0x00, 0x07);
+  delay(2000);
+  display.startscrolldiagleft(0x00, 0x07);
+  delay(2000);
+  display.stopscroll();
+}
